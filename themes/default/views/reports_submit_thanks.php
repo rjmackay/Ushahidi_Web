3ab1d0b0d4f2ecb3114695fa14d8675ec8c19c15
Dan Smith <dan@dotherightthing.co.nz>
2011-02-26 16:45:04 +1300

    Tweaked layout of report thankyou page incl adding new links (that look like buttons); added new language phrases to ui_main.php; STILL TO DO: MSIE compatibility (for whole site);

--- themes/default/views/reports_submit_thanks.php
+++ themes/default/views/reports_submit_thanks.php
@@ -7,15 +7,23 @@
 			<div class="green-box">
 				<h3><?php echo Kohana::lang('ui_main.reports_submitted');?></h3>
 
-				<div class="thanks_msg"><a href="<?php echo
-					url::site().'reports' ?>"><?php echo Kohana::lang('ui_main.reports_return');?></a><br /><br /><br />
-					<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />
+				<div class="thanks_msg">
+					<p>
+						<a class="btn_submit" href="<?php echo url::site().'reports/submit' ?>"><?php echo Kohana::lang('ui_main.reports_submit_return');?></a>
+					</p>
+					<p class="clearingfix">
+						<a class="btn_submit" href="<?php echo url::site() ?>"><?php echo Kohana::lang('ui_main.home_return');?></a>
+					</p>
+					<div class="clear"></div>
+					<!--<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />-->
 					<?php 
+					/*
 					print form::open('http://feedback.ushahidi.com/fillsurvey.php?sid=2', array('target'=>'_blank'));
 					print form::hidden('alert_code', $_SERVER['SERVER_NAME']);
 					print "&nbsp;&nbsp;";
 					print form::submit('button', Kohana::lang('ui_main.feedback'), ' class=btn_gray ');
 					print form::close();
+					*/
 					?>
 				</div>
 			</div>
