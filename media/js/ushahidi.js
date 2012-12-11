b2edf840874305c9133fd851689e05f9fc055fae
Robbie Mackay <rm@robbiemackay.com>
2012-12-12 12:01:17 +1300

    Ushahidi.js split building popup html out of Map.onFeatureSelect
    Create Map.buildPopupContent to build the html for popups
    This allows us to override popup html more easily

--- media/js/ushahidi.js
+++ media/js/ushahidi.js
@@ -792,43 +792,9 @@
 		lon = zoom_point.lon;
 		lat = zoom_point.lat;
 
-		// Image to display within the popup
-		var image = "";
-		if (event.feature.attributes.thumb !== undefined && event.feature.attributes.thumb != '') {
-			image = "<div class=\"infowindow_image\"><a href='"+event.feature.attributes.link+"'>";
-			image += "<img src=\""+event.feature.attributes.thumb+"\" height=\"59\" width=\"89\" /></a></div>";
-		} else if (event.feature.attributes.image !== undefined && event.feature.attributes.image != '') {
-			image = "<div class=\"infowindow_image\">";
-			image += "<a href=\""+event.feature.attributes.link+"\" title=\""+event.feature.attributes.name+"\">";
-			image += "<img src=\""+event.feature.attributes.image+"\" />";
-			image += "</a>";
-			image += "</div>";
-		}
-
-		var content = "<div class=\"infowindow\">" + image +
-		    "<div class=\"infowindow_content\">"+
-		    "<div class=\"infowindow_list\">"+event.feature.attributes.name+"</div>\n" +
-		    "<div class=\"infowindow_meta\">";
-
-		if (typeof(event.feature.attributes.link) != 'undefined' &&
-		    event.feature.attributes.link != '') {
-
-		    content += "<a href='"+event.feature.attributes.link+"'>" +
-			    "More Information</a><br/>";
-		}
-
-		content += "<a id=\"zoomIn\">";
-		content += "Zoom In</a>";
-		content += "&nbsp;&nbsp;|&nbsp;&nbsp;";
-		content += "<a id=\"zoomOut\">";
-		content += "Zoom Out</a></div>";
-		content += "</div><div style=\"clear:both;\"></div></div>";		
-
-		if (content.search("<script") != -1) {
-			content = "Content contained Javascript! Escaped content " +
-			    "below.<br />" + content.replace(/</g, "&lt;");
-		}
-		  
+		// Get HTML content for popup
+		content = this.buildPopupContent(event.feature);
+		
 		// Destroy existing popups before opening a new one
 		if (event.feature.popup != null) {
 			map.removePopup(event.feature.popup);
@@ -853,6 +819,57 @@
 		$("#zoomOut", popup.contentDiv).click(
 			{context: this, latitude: lat, longitude: lon, zoomFactor: -1}, 
 			this.zoomToSelectedFeature);
+	}
+	
+	/**
+	 * APIMethod: buildPopupContent
+	 *
+	 * Build html content for popups when features selected. 
+	 * Called by onFeatureSelect
+	 *
+	 * Parameters:
+	 * feature   - {Object} GeoJSON feature object
+	 */
+	Ushahidi.Map.prototype.buildPopupContent = function(feature)
+	{
+		// Image to display within the popup
+		var image = "";
+		if (feature.attributes.thumb !== undefined && feature.attributes.thumb != '') {
+			image = "<div class=\"infowindow_image\"><a href='"+feature.attributes.link+"'>";
+			image += "<img src=\""+feature.attributes.thumb+"\" height=\"59\" width=\"89\" /></a></div>";
+		} else if (feature.attributes.image !== undefined && feature.attributes.image != '') {
+			image = "<div class=\"infowindow_image\">";
+			image += "<a href=\""+feature.attributes.link+"\" title=\""+feature.attributes.name+"\">";
+			image += "<img src=\""+feature.attributes.image+"\" />";
+			image += "</a>";
+			image += "</div>";
+		}
+
+		var content = "<div class=\"infowindow\">" + image +
+		    "<div class=\"infowindow_content\">"+
+		    "<div class=\"infowindow_list\">"+feature.attributes.name+"</div>\n" +
+		    "<div class=\"infowindow_meta\">";
+
+		if (typeof(feature.attributes.link) != 'undefined' &&
+		    feature.attributes.link != '') {
+
+		    content += "<a href='"+feature.attributes.link+"'>" +
+			    "More Information</a><br/>";
+		}
+
+		content += "<a id=\"zoomIn\">";
+		content += "Zoom In</a>";
+		content += "&nbsp;&nbsp;|&nbsp;&nbsp;";
+		content += "<a id=\"zoomOut\">";
+		content += "Zoom Out</a></div>";
+		content += "</div><div style=\"clear:both;\"></div></div>";		
+
+		if (content.search("<script") != -1) {
+			content = "Content contained Javascript! Escaped content " +
+			    "below.<br />" + content.replace(/</g, "&lt;");
+		}
+		
+		return content;
 	}
 
 	/**
